3942902650d31b0c48320a00354c0734906c1d83
Joachim Strömbergson <joachim@assured.se>
2023-11-28 19:59:14 +0100

    Added code to extract relevant fields Added code to calculate frequency stats for names
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- day04/day04.py
+++ day04/day04.py
@@ -1,134 +1,74 @@
 #!/usr/bin/env python3
 #=======================================================================
-# Solutions to Advent of Code 2016, day 4.
+# Solutions to Advent of Code 2016, day 04.
 #=======================================================================
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def get_input(filename):
+    lines = []
     with open(filename,'r') as f:
         for line in f:
-            l = line.strip()
-    return l
+            lines.append(line.strip())
+    return lines
 
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
-def get_moves(s):
-    m = ""
-    for c in s:
-        if c != " ":
-            m += c
-    l = m.split(",")
+def get_room_data(lines):
+    room_data = []
 
-    moves = []
-    for c in l:
-        m = c[0]
-        d = int(c[1:])
-        moves.append((m, d))
-    return(moves)
+    for line in lines:
+        (tmp1, tmp2) = line.split('[')
+        checksum = tmp2[:-1]
 
+        tmp3 = tmp1.split('-')
+        room_id = tmp3[-1]
+        enc_name = ''.join(tmp3[:-1])
 
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def get_direction(m, curr):
-    if curr == "West":
-        if m == "L":
-            return "South"
-        if m == "R":
-            return "North"
+        room_data.append((enc_name, room_id, checksum))
 
-    if curr == "East":
-        if m == "L":
-            return "North"
-        if m == "R":
-            return "South"
-
-    if curr == "North":
-        if m == "L":
-            return "West"
-        if m == "R":
-            return "East"
-
-    if curr == "South":
-        if m == "L":
-            return "East"
-        if m == "R":
-            return "West"
+    return room_data
 
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
-def get_position(curr, d, x, y):
-    if curr == "North":
-        y = y - d
+def get_room_data_stats(room_data):
+    stats = []
 
-    if curr == "South":
-        y = y + d
-
-    if curr == "West":
-        x = x - d
-
-    if curr == "East":
-        x = x + d
-
-    return (x, y)
+    for room in room_data:
+        (room_enc_name, room_id, room_checksm) = room
+        chr_stats = {}
+        for c in room_enc_name:
+            if c in chr_stats:
+                chr_stats[c] += 1
+            else:
+                chr_stats[c] = 1
+        stats.append(chr_stats)
+    return stats
 
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def problem1():
-    my_input = get_input("day01_input.txt")
-    my_moves = get_moves(my_input)
+#    my_input = get_input("day04_input.txt")
+    my_input = get_input("day04_example.txt")
 
-    xd = 0
-    yd = 0
-    curr_dir = "North"
+    my_room_data = get_room_data(my_input)
+    my_room_data_stats = get_room_data_stats(my_room_data)
 
-    for m, d in my_moves:
-        curr_dir = get_direction(m, curr_dir)
-        (xd, yd) = get_position(curr_dir, d, xd, yd)
+    print("Room data:", my_room_data)
+    print("Room data stats:", my_room_data_stats)
 
     print("Problem1:")
-    print("Total distance: %d" % (abs(xd) + abs(yd)))
     print("")
 
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def problem2():
-#    my_input = get_input("day01_example1.txt")
-    my_input = get_input("day01_input.txt")
-    my_moves = get_moves(my_input)
-
-    # Start at pos 0,0 and add it to visited positions
-    # We face north when starting.
-    x = 0
-    y = 0
-    curr_dir = "North"
-    visited = set()
-    visited.add((x, y))
-    done = False
-
-    for (m, d) in my_moves:
-        new_dir = get_direction(m, curr_dir)
-
-        # Walk the path, generate and check all positions.
-        for delta in range(1, d + 1):
-            pos = get_position(new_dir, delta, x, y)
-
-            if pos in visited and not done:
-                revisited = pos
-                done = True
-            visited.add(pos)
-
-        # Set last delta position as new position.
-        # Update direction we are facing.
-        (x, y) = pos
-        curr_dir = new_dir
 
     print("Problem2:")
-    print("Distance to first revisited position:", (abs(revisited[0]) + abs(revisited[1])))
     print("")
 
 
