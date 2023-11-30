56362e53c3f987d7226d9f2fd95b4ef5eb1fc1c8
Joachim Strömbergson <joachim@assured.se>
2023-11-30 10:45:54 +0100

    Reworked checksum verification. Now generates correct result for problem 1
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- day04/day04.py
+++ day04/day04.py
@@ -3,6 +3,9 @@
 # Solutions to Advent of Code 2016, day 04.
 #=======================================================================
 
+from collections import Counter
+
+
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def get_input(filename):
@@ -27,50 +30,17 @@ def get_room_data(lines):
         enc_name = ''.join(tmp3[:-1])
 
         room_data.append((enc_name, room_id, checksum))
-
     return room_data
 
-
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
-def get_room_data_stats(room_data):
-    stats = []
+def calc_csum(name):
+    freq_table = Counter(''.join(sorted(name))).most_common()
+    csum = ''
 
-    for room in room_data:
-        (room_enc_name, room_id, room_checksm) = room
-        chr_stats = {}
-        for c in room_enc_name:
-            if c in chr_stats:
-                chr_stats[c] += 1
-            else:
-                chr_stats[c] = 1
-        stats.append(chr_stats)
-    return stats
-
-
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def check_chars(a, b, my_db):
-    if (a not in my_db) or (b not in my_db):
-        return False
-
-    if my_db[a] > my_db[b]:
-        return True
-
-    elif (my_db[a] == my_db[b]) and (ord(a) < ord(b)):
-        return True
-
-    else:
-        return False
-
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def real_room(csum, my_db):
-    return check_chars(csum[0], csum[1], my_db) and \
-        check_chars(csum[1], csum[2], my_db) and \
-        check_chars(csum[2], csum[3], my_db) and \
-        check_chars(csum[3], csum[4], my_db)
-
+    for i in range(5):
+        csum += freq_table[i][0]
+    return csum
 
 
 #-------------------------------------------------------------------
@@ -80,15 +50,15 @@ def problem1():
 #    my_input = get_input("day04_example.txt")
 
     my_room_data = get_room_data(my_input)
-    my_room_data_stats = get_room_data_stats(my_room_data)
 
     real_rooms = 0
     sector_sum = 0
     for i in range(len(my_room_data)):
         (name, sector_id, csum) = my_room_data[i]
-        my_db = my_room_data_stats[i]
 
-        if real_room(csum, my_db):
+        expected_csum = calc_csum(name)
+
+        if csum == expected_csum:
             real_rooms += 1
             sector_sum += int(sector_id)
             print("Room", name, sector_id, csum, "is real")
